827ebcc1a33a7386d9e796db2a5d7d3fe240bf19
Andrew Bradt <andrewjbradt@gmail.com>
2022-07-29 17:04:45 -0400

    update create script

--- npm-scripts/create.js
+++ npm-scripts/create.js
@@ -4,8 +4,8 @@ const fs = require('fs/promises');
 const TEMPLATE_DIR = path.resolve(__dirname, '..', 'templates');
 const SRC_PATH = path.resolve(__dirname, '..', 'src');
 
-const templatePath = path.resolve(TEMPLATE_DIR, process.argv[2]);
-const scriptPath = path.resolve(SRC_PATH, process.argv[3]);
+const templatePath = path.resolve(TEMPLATE_DIR, 'starter.js');
+const scriptPath = path.resolve(SRC_PATH, process.argv[2]);
 
 fs.copyFile(templatePath, scriptPath)
   .then(() => console.log('success'))
