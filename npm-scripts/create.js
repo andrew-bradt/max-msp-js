5b184db2bbfc21ad8fcafe870fce1865b010aca2
Andrew Bradt <andrewjbradt@gmail.com>
2022-07-30 17:54:55 -0400

    modify create.js script to add .js extension to argv

--- npm-scripts/create.js
+++ npm-scripts/create.js
@@ -5,7 +5,7 @@ const TEMPLATE_DIR = path.resolve(__dirname, '..', 'templates');
 const SRC_PATH = path.resolve(__dirname, '..', 'src');
 
 const templatePath = path.resolve(TEMPLATE_DIR, 'starter.js');
-const scriptPath = path.resolve(SRC_PATH, process.argv[2]);
+const scriptPath = path.resolve(SRC_PATH, `${process.argv[2]}.js`);
 
 fs.copyFile(templatePath, scriptPath)
   .then(() => console.log('success'))
